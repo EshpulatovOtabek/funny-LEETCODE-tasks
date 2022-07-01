eaf55ec6ab792ce661ad76e636d4579fa88c345c
Otabek Eshpulatov <105120956+EshpulatovOtabek@users.noreply.github.com>
2022-07-01 20:54:39 +0500

    3# LEETCODE
     the longest substring without repeating characters

--- practice.cpp
+++ practice.cpp
@@ -1,31 +1,76 @@
 
-// DATE: 7/1/2022 7:30pm
-// DEVELOPER: Eshpulatov Otabek
-// TASK: reversing a string using only swap() function
-
+// DATE: 6/30/2022
+// MADE BY: Eshpulatov Otabek
+// PURPOSE: finding the longest non-repeating substring in a string
 
 #include <iostream>
 using namespace std;
 
-string REVERSE(string num)
-{
-    for(int i=0; i<num.size()/2; i++)
-    {
-        swap(num[i], num[num.size()-1-i]); //swapping elements' positions
-    }
 
-    return num; //if you want to return only decimal values, you may change function data type and parameter values too
-}
+int TTT(string str, string s, int start);
+string fff(string str, string s, int start);
 
 
 int main()
 {
-    string num;
-    string revNum;
-    cout<<"num= "; // assigning user entered value to a string called 'num'
-    cin>>num;
-    revNum=REVERSE(num); // making sure reversed data is saved to a string
-    cout<<"result: "<<revNum<<"\n"<<endl; // displaying a result on the screen
+    string str;
+
+    cout<<">>> ";
+    cin>>str;    // getting a string from a user
+    int MAX=0;
+    string s;
+    string clone;
+    int h;
+
+    for(int i=0; i<str.size(); i++)  // looping through all non-repeating letters combination
+    {
+        s="";
+        s+=str[i];
+        int temp = i+1;
+
+        if(MAX<TTT(str,s,temp))
+        {
+            MAX=TTT(str,s,temp); // making a variable, MAX, equal to a result from functions below
+            clone=fff(str,s,temp);
+        }
+    }
+    cout<<">>> "<<MAX<<endl; // printing results
+    cout<<">>> "<<clone<<"\n"<<endl;
 
     return main();
+}
+
+
+int TTT(string str, string s, int start)
+{
+    for(int i=start; i<str.size(); i++)
+    {
+        if(s.find(str[i])>s.size())   // it goes through all letters and compares if they are unique
+        {
+            s+=str[i];
+        }
+        else  // if it is not a case, the function stops and breaks the loop
+        {
+            break;
+        }
+    }
+    return s.size(); // afterall, the function return length of all uniques letters
+}
+
+
+string fff(string str, string s, int start) // this function is the same as the first function above but it returns the longest unique letters, not the length of it
+{
+    for(int i=start; i<str.size(); i++)
+    {
+        if(s.find(str[i])>s.size())
+        {
+            s+=str[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return s;
 }
